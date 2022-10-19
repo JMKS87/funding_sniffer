8cd79a91210dc61b33fe2a7e6db09a02083f06a1
Jan Skurski <jan.skurski@gmail.com>
2022-10-19 20:27:57 +0200

    fix: Fix run_flask.sh

--- run_flask.sh
+++ run_flask.sh
@@ -1,5 +1,5 @@
-export FLASK_APP = app.py
-export FLASK_ENV = development
-export FLASK_DEBUG = 0
-source source venv/bin/activate
-python -m flask run
+export FLASK_APP=app.py
+export FLASK_ENV=development
+export FLASK_DEBUG=0
+source venv/bin/activate
+python -m flask run --host=0.0.0.0
